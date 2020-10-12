3268fd38761a0e70099eaf17d257169a77a2c417
saoussen <sawsan_sli3i@live.fr>
2020-10-12 00:55:27 +0000

    challenge of loopy lighthouse accepted

---
+++ loopy-Lighthouse.js
@@ -0,0 +1,15 @@
+for (let i=100; i<=200; i++){
+  if (i%12 === 0){
+    console.log("LoopyLighthouse \n");
+  }
+  else if(i%3 === 0){
+    console.log("Loopy \n");
+  }
+  else if (i%4 === 0){
+    console.log("Lighthouse \n");
+  }
+  else{
+    console.log(i+"\n");
+  }
+  
+}
