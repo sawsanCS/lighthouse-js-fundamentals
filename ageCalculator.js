82014c0ee397f318ceaf526b71f3b05ef43665f2
saoussen <sawsan_sli3i@live.fr>
2020-10-12 15:23:48 +0000

    I return the age of each candidate

---
+++ ageCalculator.js
@@ -0,0 +1,6 @@
+const ageCalculator = function (name, B_year, C_year){
+return name+"is "+ (C_year - B_year)+" years old.";
+}
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
